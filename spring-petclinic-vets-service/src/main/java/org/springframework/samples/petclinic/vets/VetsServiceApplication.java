e3f22289ef80aa905769c5492da8591336b1674e
Đinh Đức Thiện <ducthien19012004@gmail.com>
2025-04-09 12:22:11 +0000

    fix: update to run jenkins

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -30,6 +30,7 @@ import org.springframework.samples.petclinic.vets.system.VetsProperties;
 public class VetsServiceApplication {
 
 	public static void main(String[] args) {
+		System.println.out("aaaaa")
 		SpringApplication.run(VetsServiceApplication.class, args);
 	}
 }
