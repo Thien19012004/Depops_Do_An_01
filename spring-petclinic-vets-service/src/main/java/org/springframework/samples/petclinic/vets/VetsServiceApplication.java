3b0f5cd7ff1d383bd59cb7fa4cea03799b63fbef
root <root@DESKTOP-P0LSD89.>
2025-04-09 12:29:22 +0000

    Fix: add to run jenkins

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -30,7 +30,7 @@ import org.springframework.samples.petclinic.vets.system.VetsProperties;
 public class VetsServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("aaaaaaaa");
+		System.out.println("aaaaaaaaaaaaa");
 		SpringApplication.run(VetsServiceApplication.class, args);
 	}
 }
